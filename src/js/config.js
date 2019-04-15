fbe7ad97467397ea9fdb49de2aaf761420cb5ff7
Kyle Gallagher <litonfiredesign@gmail.com>
2019-04-15 03:30:36 -0400

    changed dummy key

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 const keys = {
-    keysArr: ['f17ed734666679f72c8ebe6sd@157ab1'], // put your key here
+    keysArr: ['f17ed734666679f72c8ebe6sd3157ab1'], // put your key here
 }
 export const current = keys.keysArr[0];
